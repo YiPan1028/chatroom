67b87ef0bcb4e863fbd646f9afe54d580d05333a
panyi16 <elevenyi.pan@mail.utoronto.ca>
2025-02-17 13:01:44 -0500

    'User entity'

--- src/main/java/entity/CommonUser.java
+++ src/main/java/entity/CommonUser.java
@@ -1,26 +1,34 @@
 package entity;
 
+import java.util.UUID;
+
 /**
  * A simple implementation of the User interface.
  */
 public class CommonUser implements User {
 
-    private final String name;
+    private final String username;
     private final String password;
+    private final String userID;
 
     public CommonUser(String name, String password) {
-        this.name = name;
+        this.username = name;
         this.password = password;
+        this.userID = UUID.randomUUID().toString();;
     }
 
     @Override
     public String getName() {
-        return name;
+        return username;
     }
 
     @Override
     public String getPassword() {
         return password;
+    }
+
+    public String getUserID() {
+        return userID;
     }
 
 }
