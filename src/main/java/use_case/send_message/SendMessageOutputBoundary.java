fd2acdde2fac84ded4dddae6d3c7ca4609373f70
linhaoli <lilinhao.li@mail.utoronto.ca>
2025-02-18 11:38:19 -0500

    output boundary

--- src/main/java/use_case/send_message/SendMessageOutputBoundary.java
+++ src/main/java/use_case/send_message/SendMessageOutputBoundary.java
@@ -1,4 +1,8 @@
 package use_case.send_message;
 
 public interface SendMessageOutputBoundary {
+    void presentSendMessageResponse(SendMessageResponseDTO response);
+    void presentChatHistoryResponse(ChatHistoryResponseDTO response);
+    void presentChatRoomResponse(ChatRoomResponseDTO response);
+
 }
